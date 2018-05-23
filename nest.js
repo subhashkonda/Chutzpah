2b98556c22cd6a77399694930696f812b32c2743
subhashkonda <subhashkonda@gmail.com>
2018-05-24 00:45:50 +0530

    Add files via upload

---
+++ nest.js
@@ -0,0 +1,10 @@
+var ABC = (function () {
+    function ABC(content) {
+        this.content = content;
+        this.vowels = content;
+    }
+    ABC.prototype.return10 = function () {
+        return 10;
+    };
+    return ABC;
+})();
---
+++ nest.ts
@@ -0,0 +1,11 @@
+
+class ABC {
+    vowels: string;
+    constructor(public content: string) {
+        this.vowels = content;
+    }
+
+    return10() {
+        return 10;
+    }
+}
